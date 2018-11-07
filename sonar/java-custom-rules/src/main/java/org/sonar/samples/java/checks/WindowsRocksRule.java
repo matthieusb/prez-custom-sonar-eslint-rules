0de6355a6fef535e450e50f3f826cfd716267940
Nicolas Achereiner <nicolas.achereiner-ext@isocel.info>
2018-11-07 15:10:35 +0100

    feat: creation de la regle WindowsRocks

---
+++ sonar/java-custom-rules/src/main/java/org/sonar/samples/java/checks/WindowsRocksRule.java
@@ -0,0 +1,24 @@
+package org.sonar.samples.java.checks;
+
+import java.util.List;
+import org.sonar.check.Rule;
+import org.sonar.check.RuleProperty;
+import org.sonar.plugins.java.api.IssuableSubscriptionVisitor;
+import org.sonar.plugins.java.api.JavaFileScanner;
+import org.sonar.plugins.java.api.JavaFileScannerContext;
+import org.sonar.plugins.java.api.tree.AnnotationTree;
+import org.sonar.plugins.java.api.tree.BaseTreeVisitor;
+import org.sonar.plugins.java.api.tree.IdentifierTree;
+import org.sonar.plugins.java.api.tree.MethodTree;
+import org.sonar.plugins.java.api.tree.Tree;
+import org.sonar.plugins.java.api.tree.Tree.Kind;
+
+@Rule(key = "WindowsRocks")
+public class WindowsRocksRule extends IssuableSubscriptionVisitor {
+
+	@Override
+	public List<Kind> nodesToVisit() {
+		return null;
+	}
+
+}

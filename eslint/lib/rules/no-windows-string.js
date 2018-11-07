0f2b8ae5e7cb43f45a78fe12235b1e48bf7d73f0
matthieusb <sauboua.beneluz.matthieu@gmail.com>
2018-11-07 15:31:30 +0100

    feat: règle init avec RuleTester configuré

---
+++ eslint/lib/rules/no-windows-string.js
@@ -0,0 +1,44 @@
+/**
+ * @fileoverview Interdire de parler de Windows
+ * @author matthieusb
+ */
+"use strict";
+
+//------------------------------------------------------------------------------
+// Rule Definition
+//------------------------------------------------------------------------------
+
+module.exports = {
+    meta: {
+        docs: {
+            description: "Interdire de parler de Windows",
+            category: "Fill me in",
+            recommended: false
+        },
+        fixable: null,  // or "code" or "whitespace"
+        schema: [
+            // fill in your schema
+        ]
+    },
+
+    create: function(context) {
+
+        // variables should be defined here
+
+        //----------------------------------------------------------------------
+        // Helpers
+        //----------------------------------------------------------------------
+
+        // any helper functions should go here or else delete this section
+
+        //----------------------------------------------------------------------
+        // Public
+        //----------------------------------------------------------------------
+
+        return {
+
+            // give me methods
+
+        };
+    }
+};
